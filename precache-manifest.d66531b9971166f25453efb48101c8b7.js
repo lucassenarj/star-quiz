6dea82b2e4d9e9cbf67c849c53eb228348e9c0b1
Lucas Sena <lucassena.rj@gmail.com>
2019-08-01 10:35:27 +0100

    Updates

---
+++ precache-manifest.d66531b9971166f25453efb48101c8b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "987f85e589566c66b37e",
+    "url": "/star-quiz/static/js/runtime~main.987f85e5.js"
+  },
+  {
+    "revision": "8cc9ebdb4d334838c1c6",
+    "url": "/star-quiz/static/js/main.8cc9ebdb.chunk.js"
+  },
+  {
+    "revision": "1685899ec064bea9132b",
+    "url": "/star-quiz/static/js/1.1685899e.chunk.js"
+  },
+  {
+    "revision": "1685899ec064bea9132b",
+    "url": "/star-quiz/static/css/1.8623d0e6.chunk.css"
+  },
+  {
+    "revision": "ea830adba2cfd6194c6d17f319625553",
+    "url": "/star-quiz/index.html"
+  }
+];
